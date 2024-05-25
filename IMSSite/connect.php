80137c79b9788bc2ffe88170d33c99687ef3f874
saleh kanaan <saleh.kanaan322@gmail.com>
2024-05-25 15:25:21 +0300

    n

--- IMSSite/connect.php
+++ IMSSite/connect.php
@@ -1,22 +1,10 @@
 <?php
- /* Connect to the database
- $host="sql6.freesqldatabase.com";
- $user="sql6692867";
- $password="cu8mVDprys"; // this field is equal to root for MAC users
- $db = "sql6692867";
 
-// Create connection
-$conn = mysqli_connect($host, $user, $password, $db);
+  $server="localhost";
+  $user="id22050951_imsite";
+  $password="Imsite@2024";
+  $db = "id22050951_ims";
   
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }*/
-
-$server="localhost";
-$user="root";
-$password="";
-$db = "test";
 
 $conn = mysqli_connect($server,$user,$password,$db);
 if (mysqli_connect_errno())
